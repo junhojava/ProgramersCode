ba8a7841def3385a0c13a441cc92071dbe19d7cb
junhojava <junho9341@gmail.com>
2020-08-26 00:07:48 +0900

    테스트 케이스는 통과하는데, 실행하면 실패함.

--- src/main/java/code/programers/level/one/CrainSolution.java
+++ src/main/java/code/programers/level/one/CrainSolution.java
@@ -1,17 +1,113 @@
 package code.programers.level.one;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
 public class CrainSolution 
 {
+    List<Integer> bucket;
+
+
     public int solution(int[][] board, int[] moves)
     {
-        int answer = 4;
+        int answer = 0;
+        bucket = new ArrayList<Integer>();
+
+        for(int move: moves)
+        {
+            int top = findDollPosition(board[move-1]);
+            int doll= 0;
+            int bucketOnTopDoll = bucketOnTop();
+            int bucketOnSecondDOll = bucketOnSecond();
+
+            if(top > -1)
+            {
+                doll = board[move-1][top];
+                board[move-1][top]= 0;
+            }
+
+            if(!isEmptySpace(doll))
+            {
+                bucket.add(doll);
+            }
+
+            if(bucketHaveManyTwoDolls())
+            {
+                if(bucketSameDools())
+                {
+                    bucket.remove(bucket.size()-1);
+                    bucket.remove(bucket.size()-1);
+                    answer += 2;
+                }
+            }
+        }
 
         return answer;
+    }
+
+    int findDollPosition(int[] board)
+    {
+        int result= -1;
+
+        for(int index=board.length-1; index>-1 ; index--)
+        {
+            if(!isEmptySpace(board[index]))
+            {
+                result = index;
+
+                return result;
+            }
+        }
+        return result;
+    }
+
+    boolean isEmptySpace(int value)
+    {
+        boolean result = false;
+
+        if(value == 0)
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+
+        return result;
+    }
+
+    int bucketOnTop()
+    {
+        if(bucket.size() == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return bucket.get(bucket.size()-1);
+        }
+    }
+
+    int bucketOnSecond()
+    {
+        if(bucket.size() == 0 || bucket.size() == 1)
+        {
+            return 0;
+        }
+        else
+        {
+            return bucket.get(bucket.size()-2);
+        }
+    }
+
+    boolean bucketHaveManyTwoDolls()
+    {
+        return (bucket.size()>1);
+    }
+
+    boolean bucketSameDools()
+    {
+        return (bucketOnTop() == bucketOnSecond());
     }
 }
--- src/test/java/code/programers/level/one/CrainTest.java
+++ src/test/java/code/programers/level/one/CrainTest.java
@@ -3,6 +3,8 @@ package code.programers.level.one;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 /**
  *  인형은 가장 아래 칸부터 쌓임(1x1 공간 사용)
@@ -26,18 +28,60 @@ import org.junit.jupiter.api.Test;
 public class CrainTest
 {
 
-    @Test
-    public void testCrain()
-    {
-        int[][] board = {{0,0,0,0,0}
+    int[][] board = {{0,0,0,0,0}
         , {0,0,1,0,3}
         , {0,2,5,0,1}
         , {4,2,4,4,2}
         , {3,5,1,3,1}};
-        int[] moves = {1,5,3,5,1,2,1,4};
+    int[] moves = {1,5,3,5,1,2,1,4};
+    CrainSolution crain = new CrainSolution();
 
-        CrainSolution crain = new CrainSolution();
-
+    @Test
+    public void testCrain()
+    {
         assertEquals(4, crain.solution(board, moves));
+    }
+    
+    @Test
+    public void testDollPosition()
+    {
+        int position = crain.findDollPosition(board[1]);
+        assertEquals(4, position);
+    }
+
+    @Test
+    public void testBucketOnTop()
+    {
+        crain.bucket = new ArrayList<Integer>();
+        crain.bucket.add(3);
+        int doll = crain.bucketOnTop();
+
+        assertEquals(3, doll);
+    }
+
+    @Test
+    public void testBucketOnSecond()
+    {
+        crain.bucket = new ArrayList<Integer>();
+        crain.bucket.add(3);
+        int empty = crain.bucketOnSecond();
+
+        assertEquals(0, empty);
+
+        crain.bucket.add(4);
+        int doll = crain.bucketOnSecond();
+
+        assertEquals(3, doll);
+    }
+
+    @Test
+    public void testBucketSameDolls()
+    {
+        crain.bucket = new ArrayList<Integer>();
+        crain.bucket.add(3);
+        crain.bucket.add(3);
+
+        assertEquals(true, crain.bucketSameDools());
+
     }
 }
