1a0c066d23ff1fc5364dd8a13af46f95a9861dd2
junhojava <junho9341@gmail.com>
2020-08-25 21:56:17 +0900

    주석 표기 변경, 작동 위치를 다시 지정하는 메서드 실행

--- src/test/java/code/programers/level/one/CrainTest.java
+++ src/test/java/code/programers/level/one/CrainTest.java
@@ -4,24 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 /**
- *  1X1 칸
  *  인형은 가장 아래 칸부터 쌓임(1x1 공간 사용)
- *  크레인은 좌우로 움직일 수 있고, 맨 위의 인형을 집어 올림
- *  바구니에는 인형이 맨 밑부터 쌓임
- *  바구니에 같은 인형이 연속해서 중복으로 쌓이면 바구니에서 사라짐
- *  크레인이 인형이 없는곳에 집어올리는 행위를 했을 때 아무 일도 일어나지 않음.
- *  바구니 크기 == 모든 인형의 크기
- *  board == 격자의 상태(2차원 배열)
- *  moves== 크레인을 작동시킨 위치가 담긴 배열(배열)
+ *  크레인은 맨 위의 인형을 집어 올림
+ *  바구니에는 인형이 맨 밑부터 쌓이고, 2개가 중복되면 사라짐.
+ *  board == 맵, moves == 크레인 작동 위치
  * 
  *  board 안의 값은 0~100
  *  0은 빈값, 1~100은 인형
- *  크레인 이동 == 1,5,3,5,1,2,1,4
- *  바구니 상태
- *  크레인은 맨 위의 것을 빼내므로 배열의 가장 뒤의 값을 빼낸다.
- *  0(빈공간), (1, 1), (3, 0(빈공간), 3), 0, 2
- *  터진 인형 정보: 1인형 2개가, 3인형 2개
- *  남는 인형 정보: 2인형
+ * 
+ *  테스트 정보
+ *  터진 인형 개수= 1번 인형 2개, 3번 인형 2개
+ *  남는 인형 정보= 2번 인형
+ * 
+ *  다음과 같이 명명한다.
+ *  board = 맵
+ *  moves = 작동위치
+ *  stack = 바구니
+ *  
  */
 public class CrainTest
 {
@@ -49,11 +48,21 @@ public class CrainTest
         , {0,2,5,0,1}
         , {4,2,4,4,2}
         , {3,5,1,3,1}};
-        int[] moves = {1,5,3,5,1,2,1,4};
 
         CrainSolution crain = new CrainSolution();
         boolean result= crain.emptyPosition(board[0]);
+        boolean second_position = crain.emptyPosition(board[1]);
 
         assertEquals(true, result);
+        assertEquals(false, second_position);
+    }
+
+    @Test
+    public void testReplaceMoves()
+    {
+        int[] moves = {1,5,3,5,1,2,1,4};
+
+        CrainSolution crain = new CrainSolution();
+        crain.replaceMoves(moves);
     }
 }
