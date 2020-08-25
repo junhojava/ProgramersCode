90d9cd294a4a4ae7fc036fd0d2be5524df73463d
junhojava <junho9341@gmail.com>
2020-08-25 19:42:54 +0900

    board의 공간이 전부 비었는지 검사하는 메서드 구현

--- src/main/java/code/programers/level/one/CrainSolution.java
+++ src/main/java/code/programers/level/one/CrainSolution.java
@@ -16,8 +16,25 @@ public class CrainSolution
     boolean emptyPosition(int[] board)
     {
         boolean result= true;
+        int flag = 0;
 
-        
+        for(int index=0; index< board.length; index++)
+        {
+            if(board[index] == 0)
+            {
+                flag += 1;
+            }
+        }
+
+        if(flag == board.length)
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+
         return result;
     }
 }
