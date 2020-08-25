1a32ae35d28077cbb9c784e7c0f40cc66d485fbd
junho <49608580+junhojava@users.noreply.github.com>
2020-08-25 18:36:15 +0900

    기초 파일 업로드

---
+++ src/main/java/code/programers/level/CrainSolution.java
@@ -0,0 +1,13 @@
+package code.programers.level;
+
+/**
+ * Hello world!
+ *
+ */
+public class CrainSolution 
+{
+    public static void solution( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/code/programers/level/CrainTest.java
@@ -0,0 +1,14 @@
+package code.programers.level;
+
+import org.junit.jupiter.api.Test;
+/**
+ * Unit test for simple App.
+ */
+public class CrainTest
+{
+    @Test
+    public void testCrain()
+    {
+        
+    }
+}
