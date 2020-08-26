1eaf5a7095db55bf68ad9e2cd17a1d7f4d6a5320
junhojava <junho9341@gmail.com>
2020-08-26 11:18:44 +0900

    마라톤 클래스 추가

---
+++ src/main/java/code/programers/level/one/Marathon.java
@@ -0,0 +1,8 @@
+package code.programers.level.one;
+
+class Marathon {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+}
