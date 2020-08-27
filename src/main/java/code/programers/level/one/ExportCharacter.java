52851a5ee24cd9ea27d660ebb528653340d1efd6
junhojava <junho9341@gmail.com>
2020-08-27 14:57:31 +0900

    solution.length = 8 테스트 추가

---
+++ src/main/java/code/programers/level/one/ExportCharacter.java
@@ -0,0 +1,9 @@
+package code.programers.level.one;
+
+public class ExportCharacter {
+    
+    int length(String str)
+    {
+        return 8;
+    }
+}
---
+++ src/test/java/code/programers/level/one/ExportCharacterTest.java
@@ -0,0 +1,28 @@
+package code.programers.level.one;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExportCharacterTest {
+    
+    /**
+     * 문자열의 가운데 글자를 반환하는 하시오.
+     * 문자열의 길이
+     * 홀수 = 1개의 문자를 나타냄
+     * 짝수 = 2개의 문자열을 나타냄
+     * 
+     * 1 <= 문자열의 길이 <= 100
+     */
+
+    ExportCharacter exportCharacter = new ExportCharacter();
+
+     @Test
+     public void testLength()
+     {
+
+         int result = exportCharacter.length("solution");
+
+         assertEquals(8, result);
+     }
+}
