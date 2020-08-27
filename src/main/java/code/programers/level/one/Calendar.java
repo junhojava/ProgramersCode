27f792c73fbc60a53d20049e37bb9c581c6fa94a
junhojava <junho9341@gmail.com>
2020-08-27 13:51:21 +0900

    date 구현 추가

--- src/main/java/code/programers/level/one/Calendar.java
+++ src/main/java/code/programers/level/one/Calendar.java
@@ -2,9 +2,17 @@ package code.programers.level.one;
 
 public class Calendar {
 
+    int new_year = 5;
+
+    
     int date(int month, int day)
     {
-        return 5;
+        if(month == 1 && day == 1)
+        {
+            return new_year;
+        }
+
+        return 1;
     }
     
 }
