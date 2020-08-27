3710396fb264ba08440367d887003dce817c7971
junhojava <junho9341@gmail.com>
2020-08-27 14:05:50 +0900

    인스턴스 변수에 마지막날에 대한 배열 추가 1월부터 입력월까지 계산하는 메서드 구현

--- src/main/java/code/programers/level/one/Calendar.java
+++ src/main/java/code/programers/level/one/Calendar.java
@@ -4,7 +4,8 @@ public class Calendar {
 
     int new_year = 5;
 
-    
+    int[] lastdays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     int date(int month, int day)
     {
         if(month == 1 && day == 1)
@@ -17,6 +18,11 @@ public class Calendar {
 
     int convertMonthToDay(int month)
     {
-        return 120;
+        int result = 0;
+        for(int index= 0; index< month-1; index++)
+        {
+            result += lastdays[index];
+        }
+        return result;
     }
 }
