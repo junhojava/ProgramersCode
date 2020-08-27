aa66081fc57d07b8d2edf2ee8c1d05eddc641721
junhojava <junho9341@gmail.com>
2020-08-27 15:26:48 +0900

    orgranized import

--- src/test/java/code/programers/level/one/CrainTest.java
+++ src/test/java/code/programers/level/one/CrainTest.java
@@ -1,7 +1,6 @@
 package code.programers.level.one;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.ArrayList;
 
