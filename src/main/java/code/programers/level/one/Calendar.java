55f7f05d73f38c4d3f189c85af2d8444d1ec85cf
junhojava <junho9341@gmail.com>
2020-08-27 14:16:14 +0900

    2016년 해당 날짜를 계산하는 메서드 구현

--- src/main/java/code/programers/level/one/Calendar.java
+++ src/main/java/code/programers/level/one/Calendar.java
@@ -6,14 +6,27 @@ public class Calendar {
 
     int[] lastdays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+    String[] daystr = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
+    String dayConvertString(int day)
+    {
+        return daystr[day];
+    }
+
     int date(int month, int day)
     {
         if(month == 1 && day == 1)
         {
             return new_year;
         }
+        else
+        {
+            day += convertMonthToDay(month);
+            day += new_year - 1;
+            day %= 7;
+        }
 
-        return 1;
+        return day % 7;
     }
 
     int convertMonthToDay(int month)
