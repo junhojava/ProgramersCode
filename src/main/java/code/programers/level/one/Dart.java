593db14d4550ca9dff8ae8c3d0414cafeb16e51f
junhojava <junho9341@gmail.com>
2020-08-31 16:56:46 +0900

    문자를 반환하는 메서드 구현

--- src/main/java/code/programers/level/one/Dart.java
+++ src/main/java/code/programers/level/one/Dart.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dart {
+
+    String area= "SDT";
+
     int[] numbers(String input)
     {
         int result[] = new int[3];
@@ -19,22 +22,7 @@ public class Dart {
         return result;
     }
 
-    String[] area(String input)
-    {
-        String result[] = new String[3];
-        int count = 0;
-
-        for(String alphabet:input.split("[^SDT]"))
-        {
-            if(!alphabet.equals(""))
-            {
-                result[count++] = alphabet;
-            }
-        }
-        return result;
-    }
-
-    String[] special(String input)
+    String[] character(String input)
     {
         String result[] = new String[3];
         int count = 0;
@@ -47,5 +35,17 @@ public class Dart {
             }
         }
         return result;
+    }
+
+    int calc(int number, String character)
+    {
+        int length = area.indexOf(character);
+
+        for(int index=0, value = number; index<length; index++)
+        {
+            number *= value;
+        }
+
+        return number;
     }
 }
