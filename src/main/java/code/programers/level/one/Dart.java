218677e14408815942ab8c18e08931157799e1a9
junhojava <junho9341@gmail.com>
2020-08-31 16:44:42 +0900

    구역을 정보를 반환하는 기능 구현

--- src/main/java/code/programers/level/one/Dart.java
+++ src/main/java/code/programers/level/one/Dart.java
@@ -18,4 +18,34 @@ public class Dart {
 
         return result;
     }
+
+    String[] area(String input)
+    {
+        String result[] = new String[3];
+        int count = 0;
+
+        for(String alphabet:input.split("[^SDT]"))
+        {
+            if(!alphabet.equals(""))
+            {
+                result[count++] = alphabet;
+            }
+        }
+        return result;
+    }
+
+    String[] special(String input)
+    {
+        String result[] = new String[3];
+        int count = 0;
+
+        for(String alphabet:input.split("\\d"))
+        {
+            if(!alphabet.equals(""))
+            {
+                result[count++] = alphabet;
+            }
+        }
+        return result;
+    }
 }
