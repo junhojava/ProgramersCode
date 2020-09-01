0c3a1ca36a59b6f478e86f1707bcb40fe9175cc3
junhojava <junho9341@gmail.com>
2020-09-01 12:05:32 +0900

    프린터 테스트 클래스 추가

--- src/test/java/code/programers/level/two/PrinterTest.java
+++ src/test/java/code/programers/level/two/PrinterTest.java
@@ -1,5 +1,14 @@
 package code.programers.level.two;
 
+import org.junit.jupiter.api.Test;
+
 public class PrinterTest {
-    
+    /**
+     * 중요도의 값이 높을수록 중요하다.
+     * 나의 문서 location
+     * 문서 중요도 정보 priorities
+     * 내 문서의 인쇄 순번 return
+     * 
+     * • 테스트 목록
+     */
 }
