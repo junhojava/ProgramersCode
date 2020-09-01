f10e4ed9cbf02ddce5978ec387bc9e2a74100e72
junhojava <junho9341@gmail.com>
2020-09-01 13:09:53 +0900

    입력 인덱스와 최대값 인덱스를 비교해서 반환하는 메서드 추가

--- src/main/java/code/programers/level/two/Printer.java
+++ src/main/java/code/programers/level/two/Printer.java
@@ -7,7 +7,7 @@ public class Printer {
         int max = 0;
         int position = 0;
 
-        for(int index=0; index < 4; index++)
+        for(int index=0; index < numbers.length; index++)
         {
             if(max < numbers[index])
             {
@@ -16,5 +16,21 @@ public class Printer {
             }
         }
         return position;
+    }
+
+    int calcShiftedIndex(int location, int max_index, int length)
+    {
+        if(location> max_index)
+        {
+            return location - max_index;
+        }
+        else if(location < max_index)
+        {
+            return length - max_index + location;
+        }
+        else
+        {
+            return 0;
+        }
     }
 }
