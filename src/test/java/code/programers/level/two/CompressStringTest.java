60a4c9d85cb407d525235c9bf4f8b15a89ecf96d
junhojava <junho9341@gmail.com>
2020-09-01 18:14:45 +0900

    문자열을 자리수 만큼 자르는 테스트 추가

--- src/test/java/code/programers/level/two/CompressStringTest.java
+++ src/test/java/code/programers/level/two/CompressStringTest.java
@@ -1,6 +1,14 @@
 package code.programers.level.two;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +19,7 @@ public class CompressStringTest {
      * ababcdcdababcdcd(16) = (ababcdcd)(ababcdcd) = 2ababcdcd(9)
      * abcabcdede(10) = (abcabc)dede = 2abcdede(8)
      * abcabcabcabcdededededede(24) = (abcabc)(abcabc)(dedede)(dedede) = 2abcabc2dedede (14)
+     * xababcdcdababcdcd(17) = xababcdcdababcdcd(17)
      */
     CompressString compressString = new CompressString();
 
@@ -26,7 +35,37 @@ public class CompressStringTest {
     void testIsEven()
     {
         String s= "aabbaccc";
+        String s2 = "xababcdcdababcdcd";
 
-        compressString.isEven(s.length());
+        assertTrue(compressString.isEven(s.length()));
+        assertFalse(compressString.isEven(s2.length()));
+    }
+
+    @Test
+    void testCalc()
+    {
+        String s = "aabbaccc";
+        List<String> string_list = new ArrayList<String>();
+
+        List<String> result = compressString.splitAt(s, 3, s.length());
+
+        string_list.add("a");
+        string_list.add("a");
+        string_list.add("b");
+        string_list.add("b");
+        string_list.add("a");
+        string_list.add("c");
+        string_list.add("c");
+        string_list.add("c");
+
+        assertEquals(result, string_list);
+    }
+
+    @Test
+    void testIsRemeined()
+    {
+        String s ="aabbaccc";
+
+        assertTrue(compressString.isRemained(s.length(), 3));
     }
 }
