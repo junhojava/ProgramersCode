73e4ff0284dce744f6e4d37641f9b75a2e317a06
junhojava <junho9341@gmail.com>
2020-09-01 12:18:16 +0900

    0번째 인덱스의 배열을 2번 움직이도록 하는 테스트 추가

--- src/test/java/code/programers/level/two/PrinterTest.java
+++ src/test/java/code/programers/level/two/PrinterTest.java
@@ -10,5 +10,19 @@ public class PrinterTest {
      * 내 문서의 인쇄 순번 return
      * 
      * • 테스트 목록
+     * 0번째 인덱스의 배열을 2번 움직이도록 순환시킨다.
+     * 
+     * 
+     * 
+     * 
      */
+    Printer printer = new Printer();
+
+    @Test
+    void testShiftLotate()
+    {
+        int[] priorities = {2,1,3,2};
+
+        assertEquals(new int{3,2,2,1}, printer.shiftLotate(0, 2));
+    }
 }
