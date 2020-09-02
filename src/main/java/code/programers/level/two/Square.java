e79b0d90dcf829e32b97cb98934a2cbb99a64007
junhojava <junho9341@gmail.com>
2020-09-02 17:25:20 +0900

    짝수이면 True를 반환하는 메서드 구현

--- src/main/java/code/programers/level/two/Square.java
+++ src/main/java/code/programers/level/two/Square.java
@@ -6,4 +6,9 @@ public class Square {
     {
         return width == height;
     }
+
+    boolean isEven(int value)
+    {
+        return value % 2 == 0;
+    }
 }
--- src/test/java/code/programers/level/two/SquareTest.java
+++ src/test/java/code/programers/level/two/SquareTest.java
@@ -1,6 +1,7 @@
 package code.programers.level.two;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
