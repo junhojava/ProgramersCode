5774aeabcb58fa9d568179ef33a96b3a8be60f51
junhojava <junho9341@gmail.com>
2020-09-02 15:33:28 +0900

    width == height ? 테스트 추가

---
+++ src/main/java/code/programers/level/two/Square.java
@@ -0,0 +1,9 @@
+package code.programers.level.two;
+
+public class Square {
+    
+    boolean isEqualSize(int width, int height)
+    {
+        return width == height;
+    }
+}
---
+++ src/test/java/code/programers/level/two/SquareTest.java
@@ -0,0 +1,18 @@
+package code.programers.level.two;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SquareTest {
+
+    Square square = new Square();
+    int w;
+    int h;
+    @Test
+    void testEqualSize()
+    {
+        assertTrue(square.isEqualSize(10, 10));
+    }
+}
