faf4f32dd070f83f2da3231e642a7e08cc605a34
junhojava <junho9341@gmail.com>
2020-09-03 14:56:46 +0900

    테스트 통과

--- src/main/java/code/programers/level/two/Translation.java
+++ src/main/java/code/programers/level/two/Translation.java
@@ -4,6 +4,80 @@ public class Translation {
     
     boolean isBalance(String value)
     {
+        int open = value.replace("(", "").length();
+        int close = value.replace(")", "").length();
+
+        return open == close;
+    }
+
+    boolean isEmpty(String value)
+    {
+        return value.equals("");
+    }
+
+    boolean isRight(String value)
+    {
+        int count = 0;
+        for(int index=0; index<value.length(); index++)
+        {
+            if(value.substring(index, index+1).equals("("))
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+
+            if(count < 0)
+            {
+                return false;
+            }
+        }
         return true;
+    }
+
+    String reverseString(String value)
+    {
+        return value.replace("(", ",").replace(")", "(").replace(",", ")");
+    }
+
+    String solution(String s)
+    {
+        int index = 2;
+        String result = "";
+        String empty = "(";
+        String u, v;
+
+        if(isEmpty(s))
+        {
+            return s;
+        }
+        else
+        {
+            u = s.substring(0, index);
+            v = s.substring(index, s.length());
+        }
+
+        while(!isBalance(u))
+        {
+            index+=2;
+            u = s.substring(0, index);
+            v = s.substring(index, s.length());
+        }
+
+        if(isRight(u))
+        {
+            result = solution(v);
+            u += result;
+        }
+        else
+        {
+            result = solution(v);
+            String apend = reverseString(u.substring(1, u.length()-1));
+            empty += result + ")" + apend;
+            u = empty;
+        }
+        return u;
     }
 }
--- src/test/java/code/programers/level/two/TranslationTest.java
+++ src/test/java/code/programers/level/two/TranslationTest.java
@@ -1,5 +1,7 @@
 package code.programers.level.two;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -12,12 +14,126 @@ public class TranslationTest {
      * (()))( == 균형잡힌 괄호 문자열 , != 올바른 괄호 문자열
      * 
      * (())() == 균형잡힌 괄호 문자열 , == 올바른 괄호 문자열
+     * 
+     * 
+     * if(isEmpty(input));
+     * 
+     * u == 균형잡힌 괄호 문자열?
+     * v == 빈 문자열?
+     * 
+     * u == 올바른 괄호 문자열
+     * 
+     * while(!(v.length() == 0))
+     *     if(isEmpty(v))
+     *     
+     * 
+     * ※ 균형잡힌 괄호 문자열의 예시
+     *    () 혹은 )(
+     * 
+     * 
+     * ※ 입출력 예 2
+     *  s= ")("
+     * 
+     *  s는 균형잡힌 괄호 문자열
+     * 
+     * 1. s는 빈 문자열이 아닙니다.
+     * 2. u = ")(", v = ""
+     * 3. u는 올바른 문자열이 아닙니다.
+     * 4. 빈문자열 "("
+     * 5. 문자열 v는 비었습니다.
+     * 6. 빈문자열이었던 "("에 ")"를 붙입니다.
+     * 7. u의 첫번째와 마지막을 제거합니다. u = ""
+     * 8. 나머지 괄호 방향을 바꿉니다. u = ""
+     * 9. "("+ "" + ")"+ ""
+     * 10. "()"
+     * 
+     * ※ 입출력 예 3
+     * s = "()))((()"
+     * 
+     * s는 균형잡힌 괄호 문자열
+     * 
+     * 1. s는 빈 문자열이 아닙니다.
+     * 2. u= "()" v="))((()"
+     * 3. u는 올바른 괄호 문자열입니다.
+     * 4. v에 대해 1단계부터 다시 수행합니다.
+     * 5. v는 빈 문자열이 아닙니다.
+     * 6. u="))((", v="()"
+     * 7. u는 올바른 괄호 문자열이 아닙니다.
+     * 8. 빈 문자열에 첫번째 문자로 "("를 붙입니다.
+     * 9. v에 대해 1단계를 수행합니다.
+     * 10. v는 빈 문자열이 아닙니다.
+     * 11. u="()" v는 =""
+     * 12. u는 올바른 괄호 문자열입니다.
+     * 13. v는 빈 문자열입니다.
+     * 14. 빈 문자열은 "("+ "()"이 됩니다.
+     * 15. 빈 문자열에 ")"를 붙입니다. "("+ "()"+ ")"가 됩니다.
+     * 16. u의 첫 번째와 마지막 문자를 제거합니다. u=")("
+     * 17. u의 괄호방향을 바꿉니다. u="()"
+     * 18. 빈 문자열에 더합니다. "("+ "()" + ")" + "()"
+     * 19. 생성된 문자열을 반환합니다. "(())()"
+     * 20. "()"+ "(())()"이 됩니다.
+     * 
+     * ※ 입출력 예 4
+     * s = "))((()))((()"
+     * 
+     * 1. s는 빈 문자열이 아닙니다.
+     * 2. u="))((" v = "()))((()"
+     * 3. u는 올바른 괄호 문자열이 아닙니다.
+     * 4. 빈 문자열에 첫번째 문자로 "("를 붙입니다.
+     * 5. v에 대해 1단계를 수행합니다.
+     * 6. v는 빈 문자열이 아닙니다.
+     * 7. u= "()" v = "))((()"
+     * 8. .. "()"+ "(())()"이 됩니다.
+     * 9. 빈 문자열은 "("+ "()(())()"이 됩니다.
+     * 10. u의 첫번째와 마지막 문자를 제거합니다. u ="()"
+     * 11. 빈 문자열에 ")"를 더합니다. 
+     * 12. "("+ "()(())()" + ")" + "()"
+     * 13. "(()(())())()"
+     * 
      */
     Translation ts = new Translation();
 
     @Test
     void testBalnacedString()
     {
-        assertTrue(ts.isBalance("()"));
+        assertTrue(ts.isBalance("()(())"));
+    }
+
+    @Test
+    void testIsEmptyString()
+    {
+        assertTrue(ts.isEmpty(""));
+    }
+
+    @Test
+    void testIsRight()
+    {
+        assertFalse(ts.isRight(")("));
+    }
+
+    @Test
+    void testReverseString()
+    {
+        assertEquals("(())", ts.reverseString("))(("));
+    }
+
+    @Test
+    void testCaseTwo()
+    {
+        String s = ")(";
+
+        s= ts.solution(s);
+
+        assertEquals("()", s);
+    }
+
+    @Test
+    void testCaseThree()
+    {
+        String s = "()))((()";
+
+        s= ts.solution(s);
+
+        assertEquals("()(())()", s);
     }
 }
