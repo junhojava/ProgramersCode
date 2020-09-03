62447ddcdf9214d4cf3a1e894690a7dfddf2c340
junhojava <junho9341@gmail.com>
2020-09-03 21:41:47 +0900

    유니크 확인 테스트

--- src/main/java/code/programers/level/two/AlterKey.java
+++ src/main/java/code/programers/level/two/AlterKey.java
@@ -1,10 +1,52 @@
 package code.programers.level.two;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AlterKey {
+    Map<String, Integer> attribute = new HashMap<String, Integer>();
+    boolean flag = true;
+    int[][] list;
     
-    boolean isUnique(String left, String right)
+    boolean isUnique(String key)
     {
-        return left.equals(right);
+        return attribute.get(key) == null;
     }
 
+    void putKey(String key)
+    {
+        attribute.put(key, 1);
+    }
+
+    void clear()
+    {
+        flag = true;
+        attribute.clear();
+    }
+
+    int isZero(int[] numbers, int row)
+    {
+        int result = 0;
+        for(int index = 0; index< list.length; index++)
+        {
+            if(numbers[index] == 0)
+            {
+                for(int pair = 0; pair<numbers.length; pair++)
+                {
+                    if(pair != row)
+                    {
+                        list[pair][index] = 1;
+                    }
+                    else
+                    {
+                        list[pair][index] = 0;
+                    }
+                }
+                result++;
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/code/programers/level/two/AlterKeyTest.java
+++ src/test/java/code/programers/level/two/AlterKeyTest.java
@@ -1,6 +1,8 @@
 package code.programers.level.two;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +12,55 @@ public class AlterKeyTest {
     AlterKey ak = new AlterKey();
 
     @Test
-    void testDuplication()
+    void testUnique()
     {
-        assertFalse(ak.isUnique("100", "200"));
+        assertTrue(ak.isUnique("100"));
+    }
+
+    @Test
+    void testUniqueColumn()
+    {
+        int result = 0;
+        relation = new String[][]{{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+        ak.list = new int[relation[0].length][relation[0].length];
+
+        for(int column=0; column<relation[0].length; column++)
+        {
+            int row = 0;
+            while(ak.flag && row < relation.length)
+            {
+                String key = relation[row][column];
+                if(ak.isUnique(key))
+                {
+                    ak.putKey(key);
+                    row++;
+                }
+                else
+                {
+                    ak.flag = false;
+                    ak.list[0][column] = 1;
+                }
+            }
+            ak.clear();
+        }
+        result = ak.isZero(ak.list[0], 0);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void testIsPair()
+    {
+        int result = 0;
+        relation = new String[][]{{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+        ak.list = new int[][]{{0,1,1,1},{1,0,0,0},{1,0,0,0},{1,0,0,0}};
+
+
+        int index= 0;
+        if(ak.list[1][index])
+        {
+
+        }
+
+
     }
 }
