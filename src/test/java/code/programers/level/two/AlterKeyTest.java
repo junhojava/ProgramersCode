5912442f348947c8b22245097b9546b846d041ff
junhojava <junho9341@gmail.com>
2020-09-03 21:49:22 +0900

    주석문장 추가

--- src/test/java/code/programers/level/two/AlterKeyTest.java
+++ src/test/java/code/programers/level/two/AlterKeyTest.java
@@ -7,7 +7,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class AlterKeyTest {
-    
+    /**
+     * 유일성 검사
+     * 1개의 컬럼을 유일성을 검사한다
+     * 학번(유일성), 이름(중복), 전공(중복), 학년(중복)
+     * 학번을 제외한다.
+     * 카운터에 1을 더한다.
+     * 컬럼을 2개씩 짝지어 유일성을 검사한다.
+     * [이름,전공](유일), 학년(중복)
+     * 이름과 전공을 제외한다.
+     * 카운터에 1을 더한다.
+     * 
+     */
     String[][] relation;
     AlterKey ak = new AlterKey();
 
