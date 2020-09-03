b9811be67e3192a09c65262ed42bc9b76cff277a
junhojava <junho9341@gmail.com>
2020-09-03 17:21:17 +0900

    연산자의 수를 세는 함수 구현

--- src/main/java/code/programers/level/two/Expression.java
+++ src/main/java/code/programers/level/two/Expression.java
@@ -1,5 +1,20 @@
 package code.programers.level.two;
 
 public class Expression {
-    
+
+    int count;
+
+    int countOperate(String s)
+    {
+        if(s.contains("+"))
+            count++;
+
+        if(s.contains("-"))
+            count++;
+
+        if(s.contains("*"))
+            count++;
+
+        return count;
+    }
 }
