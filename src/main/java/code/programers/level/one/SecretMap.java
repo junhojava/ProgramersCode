5c00966f86505bb2de696816e102ed91c4bce2de
junhojava <junho9341@gmail.com>
2020-09-07 13:25:39 +0900

    짝수 확인 테스트 추가

--- src/main/java/code/programers/level/one/SecretMap.java
+++ src/main/java/code/programers/level/one/SecretMap.java
@@ -2,4 +2,9 @@ package code.programers.level.one;
 
 public class SecretMap {
     
+
+    boolean isEven(int number)
+    {
+        return number % 2 == 0;
+    }
 }
--- src/test/java/code/programers/level/one/SecretMapTest.java
+++ src/test/java/code/programers/level/one/SecretMapTest.java
@@ -1,5 +1,15 @@
 package code.programers.level.one;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
 public class SecretMapTest {
-    
+    SecretMap sm = new SecretMap();
+
+    @Test
+    void testIsEven()
+    {
+        assertFalse(sm.isEven(13));
+    }
 }
