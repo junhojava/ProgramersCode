c4f54a17f286a3982e85efbc5ce9177af5ec5643
junhojava <junho9341@gmail.com>
2020-09-07 22:35:16 +0900

    코드가 통과하지 못했고, 다시 시작한다.

--- src/main/java/code/programers/level/two/NewsClustrering.java
+++ src/main/java/code/programers/level/two/NewsClustrering.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class NewsClustrering {
-    
+
     boolean isZero(String str)
     {
         return str.length() == 0;
@@ -29,41 +29,104 @@ public class NewsClustrering {
         return str.matches("^[a-zA-Z]*$");
     }
 
-    String[] interSection(String[] list1, String[] list2)
+    double sets(String[] list1, String[] list2)
     {
         Map<String, Integer> map = new HashMap<String, Integer>();
-        List<String> result = new ArrayList<String>();
+        Map<String, Integer> list1_map = new HashMap<String, Integer>();
+        Map<String, Integer> list2_map = new HashMap<String, Integer>();
+        List<String> interSectionList = new ArrayList<String>();
+        List<String> unionList = new ArrayList<String>();
+        String key ="";
 
         for(String str1: list1)
         {
-            if(map.get(str1) == null)
+            if(isEnglish(str1))
             {
-                map.put(str1, 1);
-            }
-            else
-            {
-                map.put(str1, map.get(str1)+1);
+                if(list1_map.get(str1.toUpperCase())==null)
+                {
+                    list1_map.put(str1.toUpperCase(), 1);
+                }
+                else
+                {
+                    list1_map.put(str1.toUpperCase(), list1_map.get(str1.toUpperCase())+1);
+                }
+
+                if(map.get(str1.toUpperCase()) == null)
+                {
+                    map.put(str1.toUpperCase(), 1);
+                }
+                else
+                {
+                    map.put(str1.toUpperCase(), map.get(str1.toUpperCase())+1);
+                }
             }
         }
 
         for(String str2: list2)
         {
-            if(map.get(str2) == null)
+            if(isEnglish(str2))
             {
-                map.put(str2, 1);
-            }
-            else
-            {
-                map.put(str2, map.get(str2)+1);
+                if(list2_map.get(str2.toUpperCase())==null)
+                {
+                    list2_map.put(str2.toUpperCase(), 1);
+                }
+                else
+                {
+                    list2_map.put(str2.toUpperCase(), list2_map.get(str2.toUpperCase())+1);
+                }
+
+                if(map.get(str2.toUpperCase()) == null)
+                {
+                    map.put(str2.toUpperCase(), 1);
+                }
+                else
+                {
+                    map.put(str2.toUpperCase(), map.get(str2.toUpperCase())+1);
+                }
             }
         }
 
         for(Entry<String, Integer> entry: map.entrySet())
         {
+            key = entry.getKey();
+            unionList.add(entry.getKey());
             if(entry.getValue() > 1)
-                result.add(entry.getKey());
+                interSectionList.add(entry.getKey());
         }
 
-        return result.toArray(new String[0]);
+        if(map.size() == 1)
+        {
+            return jakard(list1_map.get(key), list2_map.get(key));
+        }
+        else
+        {
+            return jakard(interSectionList.size(), unionList.size());
+        }
+    }
+
+    double jakard(int number1, int number2)
+    {
+        double result;
+        if(number1 == 0 && number2 == 0)
+        {
+            result = 1;
+        }
+        else if(number1 == 0 || number2 ==0)
+        {
+            result = 0;
+        }
+        else
+        {
+            if(number2 > number1)
+            {
+                result =  (double)number1 / (double)number2;
+            }
+            else
+            {
+                result = (double)number2 / (double)number1;
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/code/programers/level/two/NewsClusteringTest.java
+++ src/test/java/code/programers/level/two/NewsClusteringTest.java
@@ -1,6 +1,7 @@
 package code.programers.level.two;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -38,10 +39,52 @@ public class NewsClusteringTest {
     void testInterSection()
     {
         String[] list1 = {"FR", "RA", "AN", "NC", "CE"};
-        String[] list2 = {"FR", "RE", "EN", "NC", "CH"};
+        String[] list2 = {"fr", "re", "en", "nc", "ch"};
 
-        String[] result = nc.interSection(list1, list2);
+        double value;
+        value = nc.sets(list1, list2);
 
-        assertArrayEquals(new String[]{"FR, NC"}, result);
+        assertEquals(0.25, value);
+    }
+
+    @Test
+    void testCaseOne()
+    {
+        String[] france = nc.combination("FRANCE");
+        String[] french = nc.combination("french");
+
+        
+        double value;
+        value = nc.sets(france, french);
+
+        assertEquals(0.25, value);
+    }
+
+    @Test
+    void testCaseTwo()
+    {
+        String[] one = nc.combination("aa1+aa2");
+        String[] two = nc.combination("AAAA12");
+
+        double value;
+        value = nc.sets(one, two);
+
+        int result = 65536;
+
+        result *= value;
+
+        assertEquals(43690, result);
+    }
+
+    @Test
+    void testCaseThree()
+    {
+        String[] one = nc.combination("E=M*C^2");
+        String[] two = nc.combination("e=m*c^2");
+        
+        double value;
+        value = nc.sets(one, two);
+
+        System.out.println(65536 * value);
     }
 }
