e6acf3d224d77482a698da4b568bc7020169525b
junhojava <junho9341@gmail.com>
2020-09-09 15:59:45 +0900

    put 구현에 필요한것들 추가

--- src/main/java/code/programers/level/two/Cache.java
+++ src/main/java/code/programers/level/two/Cache.java
@@ -1,9 +1,12 @@
 package code.programers.level.two;
 
+import java.util.Map;
+
 public class Cache {
     
     String[] buffer;
     int size;
+    Map<String, Integer> map;
 
     boolean isZero()
     {
@@ -13,8 +16,34 @@ public class Cache {
     void put(String str)
     {
         for(int index=1; index < size; index++)
+        {
             buffer[index-1] = buffer[index];
+        }
+        if(isFull())
+        {
+            map.put(str, 1);
+        }
+        else
+        {
+            if(map.get(buffer[0]) == 1)
+                map.remove(buffer[0]);
+            else
+                map.put(buffer[0], map.get(buffer[0]) -1);
+
+            map.put(str, map.getOrDefault(str, 1) + 1);
+        }
 
         buffer[size-1] = str;
+    }
+
+    boolean isFull()
+    {
+        boolean result = true;
+
+        for(String str: buffer)
+            if(str.equals(""))
+                return false;
+
+        return result;
     }
 }
