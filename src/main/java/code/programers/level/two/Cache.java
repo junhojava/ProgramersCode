2fa81f353c88afb5fbffb19f2792dba84fd7144b
junhojava <junho9341@gmail.com>
2020-09-09 16:57:19 +0900

    hit(), shift() 구현

--- src/main/java/code/programers/level/two/Cache.java
+++ src/main/java/code/programers/level/two/Cache.java
@@ -3,10 +3,11 @@ package code.programers.level.two;
 import java.util.Map;
 
 public class Cache {
-    
+
     String[] buffer;
     int size;
     Map<String, Integer> map;
+    int time;
 
     boolean isZero()
     {
@@ -15,11 +16,7 @@ public class Cache {
 
     void put(String str)
     {
-        for(int index=1; index < size; index++)
-        {
-            buffer[index-1] = buffer[index];
-        }
-        if(isFull())
+        if(!isFull())
         {
             map.put(str, 1);
         }
@@ -30,7 +27,12 @@ public class Cache {
             else
                 map.put(buffer[0], map.get(buffer[0]) -1);
 
-            map.put(str, map.getOrDefault(str, 1) + 1);
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        for(int index=1; index < size; index++)
+        {
+            buffer[index-1] = buffer[index];
         }
 
         buffer[size-1] = str;
@@ -41,9 +43,35 @@ public class Cache {
         boolean result = true;
 
         for(String str: buffer)
-            if(str.equals(""))
+            if(str == null)
                 return false;
 
         return result;
+    }
+
+    boolean isHit(String str)
+    {
+        return map.get(str) != null;
+    }
+
+    int position(String str)
+    {
+        int result = 0;
+        for(int index=0; index < size; index++)
+            if(buffer[index] != null)
+                if(buffer[index].equals(str))
+                    return index;
+
+        return result;
+    }
+
+    void shift(int number, String str)
+    {
+        for(int index = number; index < size-1; index++)
+        {
+            buffer[index] = buffer[index+1];
+        }
+
+        buffer[size-1] = str;
     }
 }
