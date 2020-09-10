5bcff3256d39e93ae3db098a10f5319c08d29faf
junhojava <junho9341@gmail.com>
2020-09-10 20:59:11 +0900

    셔틀버스 테스트 추가

---
+++ src/test/java/code/programers/level/three/ShuttleBusTest.java
@@ -0,0 +1,16 @@
+package code.programers.level.three;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ShuttleBusTest {
+
+    ShuttleBus sb = new ShuttleBus();
+
+    @Test
+    void testRemained()
+    {
+        assertEquals(1,1);
+    }
+}
