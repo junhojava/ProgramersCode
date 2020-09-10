d427733f71eaa381712c2ffc10a1c014bb56aad7
junhojava <junho9341@gmail.com>
2020-09-10 21:43:39 +0900

    버스시간표 테스트 추가

--- src/main/java/code/programers/level/three/ShuttleBus.java
+++ src/main/java/code/programers/level/three/ShuttleBus.java
@@ -1,5 +1,9 @@
 package code.programers.level.three;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ShuttleBus {
 
     int remained(int n, int m, int length)
@@ -12,5 +16,71 @@ public class ShuttleBus {
             return 0;
         else
             return 1;
+    }
+
+    String[] times(int n, int t)
+    {
+        String[] result = new String[n];
+
+        for(int index=0; index < n; index++)
+        {
+            int minute = index * t;
+            int hour = 0;
+
+            if(minute > 59)
+            {
+                hour = minute / 60;
+                minute = hour % 60;
+            }
+
+            result[index] = prefix(hour+ 9) + ":" + prefix(minute);
+
+        }
+
+        return result;
+    }
+
+    String prefix(int number)
+    {
+        if(number < 10)
+            return "0" + Integer.toString(number);
+        else
+            return Integer.toString(number);
+    }
+
+    String[][] seat(int n, int t, int m, String[] timetable)
+    {
+        String[][] result = new String[n][m];
+        String[] timelist = times(n, t);
+        Arrays.sort(timetable);
+
+        for(int index=0; index< n; index++)
+        {
+            List<String> list = new ArrayList<String>();
+            int hour = Integer.parseInt(timelist[index].split(":")[0]);
+            int minute = Integer.parseInt(timelist[index].split(":")[1]);
+
+            for(int table_index=0; table_index < timetable.length; table_index++)
+            {
+                if(!timetable[table_index].equals(""))
+                {
+                    int time_hour = Integer.parseInt(timetable[table_index].split(":")[0]);
+                    int time_minute = Integer.parseInt(timetable[table_index].split(":")[1]);
+
+                    if(hour > time_hour || (hour == time_hour && minute == time_minute))
+                    {
+                        if(list.size() > m)
+                        {
+                            list.add(timetable[table_index]);
+                            timetable[table_index] = "";
+                        }
+                    }
+                }
+            }
+
+            result[index] = list.toArray(new String[0]);
+        }
+
+        return result;
     }
 }
--- src/test/java/code/programers/level/three/ShuttleBusTest.java
+++ src/test/java/code/programers/level/three/ShuttleBusTest.java
@@ -1,5 +1,6 @@
 package code.programers.level.three;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -19,5 +20,28 @@ public class ShuttleBusTest {
         int count = sb.remained(n, m, timetable.length);
 
         assertEquals(1, count);
+    }
+
+    @Test
+    void testTimeList()
+    {
+        int n = 1;
+        int t = 1;
+
+        String[] list = sb.times(n, t);
+
+        assertArrayEquals(new String[]{"09:00"}, list);
+    }
+
+    @Test
+    void testSeatBus()
+    {
+        int n = 1;
+        int t = 1;
+        int m = 5;
+
+        String[][] list = sb.seat(n, t, m, new String[]{"08:00", "08:01", "08:02", "08:03"});
+
+        assertArrayEquals(new String[]{"08:00", "08:01", "08:02", "08:03"}, list[0]);
     }
 }
