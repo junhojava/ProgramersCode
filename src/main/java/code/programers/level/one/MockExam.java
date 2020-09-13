5889f0067b242e6a1f0ca6b9bf941aa6688d39c0
junhojava <junho9341@gmail.com>
2020-09-13 16:52:58 +0900

    최고 득점자 출력 테스트 추가

--- src/main/java/code/programers/level/one/MockExam.java
+++ src/main/java/code/programers/level/one/MockExam.java
@@ -1,5 +1,8 @@
 package code.programers.level.one;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MockExam {
     
     int index(int left, int right)
@@ -10,5 +13,32 @@ public class MockExam {
     boolean isHit(int left, int right)
     {
         return left == right;
+    }
+
+    int[] top(int[] scores)
+    {
+        int max = 0;
+        List<Integer> list = new ArrayList<Integer>();
+
+        for(int point: scores)
+            if(max<point)
+                max = point;
+
+        for(int index=0; index<scores.length; index++)
+        {
+            if(scores[index] == max)
+            {
+                list.add(index+1);
+            }
+        }
+
+        scores = new int[list.size()];
+
+        for(int index=0; index<scores.length; index++)
+        {
+            scores[index] = list.get(index);
+        }
+
+        return scores;
     }
 }
--- src/test/java/code/programers/level/one/MockExamTest.java
+++ src/test/java/code/programers/level/one/MockExamTest.java
@@ -1,5 +1,6 @@
 package code.programers.level.one;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -23,5 +24,15 @@ public class MockExamTest {
         boolean one = me.isHit(1, 1);
 
         assertTrue(one);
+    }
+
+    @Test
+    void testRank()
+    {
+        int[] rank = {2, 0, 0};
+
+        int[] top = me.top(rank);
+
+        assertArrayEquals(new int[]{1}, top);
     }
 }
