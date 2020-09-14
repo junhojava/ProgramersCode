c3b3fedfa2e4822c91969cb430e5af76bf94fa22
junhojava <junho9341@gmail.com>
2020-09-14 14:42:14 +0900

    테스트 케이스 추가

--- src/test/java/code/programers/level/one/GymClothTest.java
+++ src/test/java/code/programers/level/one/GymClothTest.java
@@ -21,4 +21,73 @@ public class GymClothTest {
     {
         assertArrayEquals(new int[] {2,4}, gc.canRent(3));
     }
+
+    @Test
+    void testCaseOne()
+    {
+        int n = 5;
+        int[] lost = {2,4};
+        int[] reserve = {1,3,5};
+
+        for(int me:reserve)
+            if(gc.who(lost, me) > -1)
+                lost[gc.who(lost, me)] = -1;
+            else
+                for(int number:gc.canRent(me))
+                    if(gc.who(lost, number) > -1)
+                    {
+                        lost[gc.who(lost, number)] = -1;
+                        break;
+                    }
+
+        n -= (lost.length - gc.count(lost));
+
+        assertEquals(5, n);
+    }
+
+    @Test
+    void testCaseTwo()
+    {
+        int n = 5;
+        int[] lost = {2,4};
+        int[] reserve = {3};
+
+        for(int me:reserve)
+            if(gc.who(lost, me) > -1)
+                lost[gc.who(lost, me)] = -1;
+            else
+                for(int number:gc.canRent(me))
+                    if(gc.who(lost, number) > -1)
+                    {
+                        lost[gc.who(lost, number)] = -1;
+                        break;
+                    }
+
+        n -= (lost.length - gc.count(lost));
+
+        assertEquals(4, n);
+    }
+
+    @Test
+    void testCaseThree()
+    {
+        int n = 3;
+        int[] lost = {3};
+        int[] reserve = {1};
+
+        for(int me:reserve)
+            if(gc.who(lost, me) > -1)
+                lost[gc.who(lost, me)] = -1;
+            else
+                for(int number:gc.canRent(me))
+                    if(gc.who(lost, number) > -1)
+                    {
+                        lost[gc.who(lost, number)] = -1;
+                        break;
+                    }
+
+        n -= (lost.length - gc.count(lost));
+
+        assertEquals(2, n);
+    }
 }
