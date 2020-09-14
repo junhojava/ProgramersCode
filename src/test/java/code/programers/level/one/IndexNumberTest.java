812ed5a694b8eeaf879391bec3f4a643a72d10d4
junhojava <junho9341@gmail.com>
2020-09-14 19:09:50 +0900

    두번째 테스트 케이스 추가

--- src/test/java/code/programers/level/one/IndexNumberTest.java
+++ src/test/java/code/programers/level/one/IndexNumberTest.java
@@ -25,4 +25,13 @@ public class IndexNumberTest {
 
         assertEquals(5, result[2]);
     }
+
+    @Test
+    void testCaseTwo()
+    {
+        int[] result = in.split(new int[]{1,5,2,6,3,7,4}, 4, 4);
+        result = in.sort(result);
+
+        assertEquals(6, result[0]);
+    }
 }
