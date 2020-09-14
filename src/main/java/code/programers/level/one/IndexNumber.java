6aa0c6eafa94b4c7a2c5165719eb20a78cf6da32
junhojava <junho9341@gmail.com>
2020-09-14 19:16:37 +0900

    사용하지 않은 메서드 제거

--- src/main/java/code/programers/level/one/IndexNumber.java
+++ src/main/java/code/programers/level/one/IndexNumber.java
@@ -3,12 +3,6 @@ package code.programers.level.one;
 import java.util.Arrays;
 
 public class IndexNumber {
-    
-    int length(int start, int end)
-    {
-        return end - start;
-    }
-
     int[] split(int[] list, int start, int end)
     {
         return Arrays.copyOfRange(list, start-1, end);
