610fa8902c2952d937d7045e151d732b5ef0b0d6
junhojava <junho9341@gmail.com>
2020-09-14 22:00:08 +0900

    내림차순 정렬 테스트 추가

--- src/main/java/code/programers/level/one/DescendSortString.java
+++ src/main/java/code/programers/level/one/DescendSortString.java
@@ -1,5 +1,8 @@
 package code.programers.level.one;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class DescendSortString {
     
     String[] split(String s)
@@ -8,6 +11,19 @@ public class DescendSortString {
 
         result[0] = s.replaceAll("[A-Z]", "");
         result[1] = s.replaceAll("[a-z]", "");
+
+        return result;
+    }
+
+    String sort(String s)
+    {
+        String[] list = s.split("");
+        String result = "";
+
+        Arrays.sort(list, Collections.reverseOrder());
+
+        for(String str:list)
+            result+= str;
 
         return result;
     }
