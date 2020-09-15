3a68dcd14b743cb81d4475823fcb15d216504107
junhojava <junho9341@gmail.com>
2020-09-15 14:16:39 +0900

    테스트케이스 1번 추가

--- src/test/java/code/programers/level/one/PrimeNumberTest.java
+++ src/test/java/code/programers/level/one/PrimeNumberTest.java
@@ -19,4 +19,27 @@ public class PrimeNumberTest {
     {
         assertEquals(0, pn.mod(2,2));
     }
+
+    @Test
+    void testCaseOne()
+    {
+        int n = 10;
+        int count = 0;
+
+        for(int left= 2; left<= n; left++)
+        {
+            count++;
+            for(int right = 2; right <= left; right++)
+            {
+                if(pn.mod(left, right) == 0)
+                {
+                    if(pn.divide(left, right) != 1)
+                        count--;
+                        break;
+                }
+            }
+        }
+
+        assertEquals(4, count);
+    }
 }
