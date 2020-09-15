3be2029b06c918e8524df08d2b2ee0a65a228a07
junhojava <junho9341@gmail.com>
2020-09-15 14:54:37 +0900

    숫자추출

--- src/test/java/code/programers/level/one/StringConvertNumberTest.java
+++ src/test/java/code/programers/level/one/StringConvertNumberTest.java
@@ -1,5 +1,6 @@
 package code.programers.level.one;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -14,5 +15,9 @@ public class StringConvertNumberTest {
         assertTrue(scn.existMinus("-1234"));
     }
 
-    
+    @Test
+    void testExtractNumber()
+    {
+        assertEquals(1234, scn.extractNumber("-1234"));
+    }
 }
