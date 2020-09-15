05f0a932c53951229884a51c45ac6935b5f299c6
junhojava <junho9341@gmail.com>
2020-09-15 16:38:33 +0900

    테스트 케이스 추가

--- src/test/java/code/programers/level/one/TrivialDivisorTest.java
+++ src/test/java/code/programers/level/one/TrivialDivisorTest.java
@@ -1,5 +1,7 @@
 package code.programers.level.one;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class TrivialDivisorTest {
@@ -10,5 +12,19 @@ public class TrivialDivisorTest {
     void testMod()
     {
         assertEquals(0,td.mod(12, 6));
+    }
+
+    @Test
+    void testCaseOne()
+    {
+        int n = 12;
+        int answer = 0;
+        for(int index= 1; index<=n; index++)
+        {
+            if(td.mod(n, index) == 0)
+                answer += index;
+        }
+
+        assertEquals(28, answer);
     }
 }
