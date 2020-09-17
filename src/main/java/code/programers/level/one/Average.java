5d715fce1e84fe896d569a0e6fd10a6ccd1699f8
junhojava <junho9341@gmail.com>
2020-09-17 14:38:13 +0900

    더하기, 배열의 길이 테스트 추가

---
+++ src/main/java/code/programers/level/one/Average.java
@@ -0,0 +1,14 @@
+package code.programers.level.one;
+
+public class Average {
+    
+    int sum(int left, int right)
+    {
+        return left + right;
+    }
+
+    int length(int[] list)
+    {
+        return list.length;
+    }
+}
---
+++ src/test/java/code/programers/level/one/AverageTest.java
@@ -0,0 +1,21 @@
+package code.programers.level.one;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AverageTest {
+    Average average = new Average();
+
+    @Test
+    void testSum()
+    {
+        assertEquals(10, average.sum(4,6));
+    }
+
+    @Test
+    void length()
+    {
+        assertEquals(2, average.length(new int[]{0, 1}));
+    }
+}
