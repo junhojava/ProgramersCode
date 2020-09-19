a51b4723078389afbed911083ea4893434a65735
junhojava <junho9341@gmail.com>
2020-09-19 16:44:31 +0900

    잘못된 테스트 변경

--- src/test/java/code/programers/level/one/RemoveMinNumberTest.java
+++ src/test/java/code/programers/level/one/RemoveMinNumberTest.java
@@ -10,35 +10,11 @@ public class RemoveMinNumberTest {
     RemoveMinNumber rmn = new RemoveMinNumber();
 
     @Test
-    void testMinIndex()
+    void testMin()
     {
         int[] list = new int[]{4,3,2,1};
-        int[] result = new int[list.length-1];
-        boolean flag = true;
-
         int min = rmn.min(list);
 
-        for(int index= 0; index<result.length; index++)
-        {
-            if(flag)
-            {
-                boolean find = rmn.findMin(list[index], min);
-                
-                if(find)
-                {
-                   flag = false;
-                }
-                else
-                {
-                    result[index] = list[index];
-                }
-            }
-            else
-            {
-                result[index] = list[index+1];
-            }
-        }
-
-        assertArrayEquals(new int[]{4,3,2}, result);
+        assertEquals(1, min);
     }
 }
