fb183254879c0b22d6596f164ce289243a5f809d
junhojava <junho9341@gmail.com>
2020-09-19 14:24:03 +0900

    문제의 해결 방법은 맞으나 메모리 공간 사용에서 문제가 발생함.

--- src/main/java/code/programers/level/one/Budget.java
+++ src/main/java/code/programers/level/one/Budget.java
@@ -1,8 +1,6 @@
 package code.programers.level.one;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Budget {
     
@@ -11,40 +9,26 @@ public class Budget {
         Arrays.sort(list);
     }
 
-    Map<String, Integer> combination(int[] list)
+    boolean isBudget(int[][] combination, int[] list, int budget)
     {
-        Map<String, Integer> result = new HashMap<String ,Integer>();
+        boolean can = false;
 
-        result.put("1", 1);
-        result.put("2", 1);
-        result.put("3", 1);
-        result.put("4", 1);
-        result.put("5", 1);
-        result.put("12", 1);
-        result.put("13", 1);
-        result.put("14", 1);
-        result.put("15", 1);
-        result.put("23", 1);
-        result.put("24", 1);
-        result.put("25", 1);
-        result.put("34", 1);
-        result.put("35", 1);
-        result.put("45", 1);
-        result.put("123", 1);
-        result.put("124", 1);
-        result.put("125", 1);
-        result.put("134", 1);
-        result.put("135", 1);
-        result.put("145", 1);
-        result.put("234", 1);
-        result.put("235", 1);
-        result.put("245", 1);
-        result.put("345", 1);
-        result.put("1234", 1);
-        result.put("1235", 1);
-        result.put("2345", 1);
-        result.put("12345", 1);
+        for(int[] dimension:combination)
+        {
+            int value = 0;
+            
+            for(int number: dimension)
+                value += list[number];
 
-        return result;
+            if(value <= budget)
+                can = true;
+        }
+
+        return can;
+    }
+
+    boolean isMax(boolean[] list, int length)
+    {
+        return list[length];
     }
 }
--- src/test/java/code/programers/level/one/BudgetTest.java
+++ src/test/java/code/programers/level/one/BudgetTest.java
@@ -3,10 +3,9 @@ package code.programers.level.one;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
+
+import code.programers.level.two.AlterKey;
 
 public class BudgetTest {
 
@@ -27,41 +26,54 @@ public class BudgetTest {
     void testCombination()
     {
         int[] list = {1,3,2,5,4};
-        budget.sort(list);
-        Map<String, Integer> map = budget.combination(list);
+        AlterKey ak = new AlterKey();
+        int[] columns = ak.columns(list.length);
+        int[][] combination = ak.dimension(columns);
 
-        Map<String, Integer> result = new HashMap<String ,Integer>();
+        boolean[] lengths = new boolean[list.length];
 
-        result.put("1", 1);
-        result.put("2", 1);
-        result.put("3", 1);
-        result.put("4", 1);
-        result.put("5", 1);
-        result.put("12", 1);
-        result.put("13", 1);
-        result.put("14", 1);
-        result.put("15", 1);
-        result.put("23", 1);
-        result.put("24", 1);
-        result.put("25", 1);
-        result.put("34", 1);
-        result.put("35", 1);
-        result.put("45", 1);
-        result.put("123", 1);
-        result.put("124", 1);
-        result.put("125", 1);
-        result.put("134", 1);
-        result.put("135", 1);
-        result.put("145", 1);
-        result.put("234", 1);
-        result.put("235", 1);
-        result.put("245", 1);
-        result.put("345", 1);
-        result.put("1234", 1);
-        result.put("1235", 1);
-        result.put("2345", 1);
-        result.put("12345", 1);
+        lengths[0] = budget.isBudget(combination, list, 9);
 
-        assertEquals(result, map);
+        int index=1;
+
+        for(; index<5; index++)
+        {
+            combination = ak.combination(columns, combination);
+            lengths[index] = budget.isBudget(combination, list, 9);
+
+            if(lengths[index] == false)
+                break;
+        }
+
+        assertEquals(3, index);
+    }
+
+    @Test
+    void testCaseTwo()
+    {
+        int[] list = {2,2,3,3};
+        AlterKey ak = new AlterKey();
+        int[] columns = ak.columns(list.length);
+        int[][] combination = ak.dimension(columns);
+
+        boolean[] lengths = new boolean[list.length];
+
+        lengths[0] = budget.isBudget(combination, list, 10);
+
+        int index=1;
+
+        for(; index<4; index++)
+        {
+            combination = ak.combination(columns, combination);
+            lengths[index] = budget.isBudget(combination, list, 10);
+
+            if(lengths[index] == false)
+                break;
+        }
+
+        if(budget.isMax(lengths, list.length-1))
+            index = list.length;
+
+        assertEquals(4, index);
     }
 }
