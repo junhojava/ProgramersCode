e95d02bc8bd560d7f897de83ca511161517fa2cb
junhojava <junho9341@gmail.com>
2020-09-22 14:58:00 +0900

    다리가 하중을 견딜 수 있는지 테스트 추가

--- src/main/java/code/programers/level/two/PassBridge.java
+++ src/main/java/code/programers/level/two/PassBridge.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 
 public class PassBridge {
     int[] trucks;
+    int weight;
 
     void ascendSort()
     {
         Arrays.sort(trucks);
+    }
+
+    boolean checkWeight(int truck)
+    {
+        return (weight - truck) >= 0;
     }
 }
--- src/test/java/code/programers/level/two/PassBridgeTest.java
+++ src/test/java/code/programers/level/two/PassBridgeTest.java
@@ -1,6 +1,7 @@
 package code.programers.level.two;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +16,15 @@ public class PassBridgeTest {
         pb.ascendSort();
 
         assertArrayEquals(new int[]{4,5,6,7}, pb.trucks);
+    }
+
+    @Test
+    void testCheckWeight()
+    {
+        pb.weight = 10;
+        pb.trucks = new int[]{7,4,5,6};
+        pb.ascendSort();
+
+        assertTrue(pb.checkWeight(pb.trucks[0]));
     }
 }
