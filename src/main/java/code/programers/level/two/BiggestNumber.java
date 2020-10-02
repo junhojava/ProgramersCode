c5382cf86a710db71f150463afbaf5aa065aebf6
junhojava <junho9341@gmail.com>
2020-10-02 15:18:42 +0900

    순열 테스트 추가

---
+++ src/main/java/code/programers/level/two/BiggestNumber.java
@@ -0,0 +1,17 @@
+package code.programers.level.two;
+
+import java.util.List;
+
+public class BiggestNumber {
+    // 순열 메서드 추가
+    int[] permutation(int[] numbers)
+    {
+        for(int index=0; index<numbers.length; index++)
+        {
+            for(int y_index=0; y_index< numbers.length; y_index++)
+            {
+                
+            }
+        }
+    }
+}
---
+++ src/test/java/code/programers/level/two/BiggestNumberTest.java
@@ -0,0 +1,17 @@
+package code.programers.level.two;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class BiggestNumberTest {
+    
+    BiggestNumber bn = new BiggestNumber();
+
+
+    @Test
+    void testPermutation()
+    {
+        
+    }
+}
