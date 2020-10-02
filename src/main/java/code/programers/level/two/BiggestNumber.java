bdf242f5e0e6a9bf76a8b812a4bce36cce72d06f
junhojava <junho9341@gmail.com>
2020-10-02 15:59:43 +0900

    가장큰수 테스트 작성

--- src/main/java/code/programers/level/two/BiggestNumber.java
+++ src/main/java/code/programers/level/two/BiggestNumber.java
@@ -1,11 +1,33 @@
 package code.programers.level.two;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class BiggestNumber {
-    // 순열 메서드 추가
-    int[] permutation()
+    List<int[]> list;
+
+    public void permutation(int[] arr, int startIdx)
     {
-        return new int[]{0123, 0132, 0213, 0231, 0312, 0321};
+        int length = arr.length;
+        if(startIdx == length-1)
+        {
+            list.add(Arrays.copyOf(arr, arr.length));
+
+            return;
+        }
+
+        for(int i=startIdx; i<length; i++)
+        {
+            swap(arr, startIdx, i);
+            permutation(arr, startIdx+1);
+            swap(arr, startIdx, i);
+        }
+    }
+
+    public void swap(int[] arr, int n1, int n2)
+    {
+        int temp = arr[n1];
+        arr[n1] = arr[n2];
+        arr[n2] = temp;
     }
 }
--- src/test/java/code/programers/level/two/BiggestNumberTest.java
+++ src/test/java/code/programers/level/two/BiggestNumberTest.java
@@ -1,6 +1,8 @@
 package code.programers.level.two;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,10 +14,25 @@ public class BiggestNumberTest {
     @Test
     void testPermutation()
     {
-        int[] array = {0123, 0132, 0213, 0231, 0312, 0321};
+        bn.list = new ArrayList<int[]>();
+        int[] numbers = {6, 10, 2};
+        int[] arr = { 0, 1, 2 };
+        bn.permutation(arr, 0);
+        int max = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        for(int[] array: bn.list)
+        {
 
-        int[] result = bn.permutation();
+            for(int index: array)
+                sb.append(numbers[index]);
 
-        assertArrayEquals(array, result);
+            if(max < Integer.parseInt(sb.toString()))
+                max = Integer.parseInt(sb.toString());
+
+            sb.setLength(0);
+        }
+
+        assertEquals(6210, max);
     }
 }
