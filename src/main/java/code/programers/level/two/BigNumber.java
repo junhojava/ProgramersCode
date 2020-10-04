0da8b60f1e6e55b84129dace5a21008b1ce50a4a
junhojava <junho9341@gmail.com>
2020-10-04 10:41:19 +0900

    가장 큰 수의 인덱스를 추출하는 테스트 추가

---
+++ src/main/java/code/programers/level/two/BigNumber.java
@@ -0,0 +1,9 @@
+package code.programers.level.two;
+
+public class BigNumber {
+    
+    int bigIndex(String number)
+    {
+        return 2;
+    }
+}
---
+++ src/test/java/code/programers/level/two/BigNumberTest.java
@@ -0,0 +1,19 @@
+package code.programers.level.two;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class BigNumberTest {
+
+    BigNumber bn = new BigNumber();
+
+    @Test
+    void testBigIndex()
+    {
+
+        int index = bn.bigIndex("192");
+
+        assertEquals(2, index);
+    }
+}
