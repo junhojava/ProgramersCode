75631ff0c370584566cc8489e76c9b635fb1d432
junhojava <junho9341@gmail.com>
2020-10-04 11:48:30 +0900

    몇개의 테스트 케이스 추가

--- src/test/java/code/programers/level/two/BigNumberTest.java
+++ src/test/java/code/programers/level/two/BigNumberTest.java
@@ -35,6 +35,104 @@ public class BigNumberTest {
             number -= index;
         }
 
+        if(start < str.length())
+            sb.append(str.substring(start , str.length()));
+
         assertEquals("94", sb.toString());
+    }
+
+    @Test
+    void testCaseTwo()
+    {
+        String str= "1231234";
+        int start = 0;
+        int number = 3;
+        StringBuilder sb = new StringBuilder();
+
+        while(number > 0)
+        {
+            int index= bn.bigIndex(str.substring(start, start+ (number+1)));
+            
+            start += index+1;
+
+            sb.append(str.substring(start-1, start));
+
+            number -= index;
+        }
+
+        if(start < str.length())
+            sb.append(str.substring(start , str.length()));
+
+        assertEquals("3234", sb.toString());
+    }
+
+    @Test
+    void testCaseThree()
+    {
+        String str= "4177252841";
+        int start = 0;
+        int number = 4;
+        StringBuilder sb = new StringBuilder();
+
+        while(number > 0)
+        {
+            int index= 0;
+            try
+            {
+                index = bn.bigIndex(str.substring(start, start+ (number+1)));
+            }
+            catch (Exception e)
+            {
+                //TODO: handle exception
+                start = str.length();
+                break;
+            }
+            
+            start += index+1;
+
+            sb.append(str.substring(start-1, start));
+
+            number -= index;
+        }
+
+        if(start < str.length())
+            sb.append(str.substring(start , str.length()));
+
+        assertEquals("775841", sb.toString());
+    }
+
+    @Test
+    void testCaseTwoelve()
+    {
+        String str= "111119";
+        int start = 0;
+        int number = 3;
+        StringBuilder sb = new StringBuilder();
+
+        while(number > 0)
+        {
+            int index= 0;
+            try
+            {
+                index = bn.bigIndex(str.substring(start, start+ (number+1)));
+            }
+            catch (Exception e)
+            {
+                //TODO: handle exception
+                start = str.length();
+                break;
+            }
+            
+            start += index+1;
+
+            sb.append(str.substring(start-1, start));
+
+            number -= index;
+        }
+
+        if(start < str.length())
+            sb.append(str.substring(start , str.length()));
+
+        assertEquals("119", sb.toString());
     }
 }
