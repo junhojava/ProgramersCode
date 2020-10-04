ffbf72825a8cf9a762a9c8f14791854471c0dfd7
junhojava <junho9341@gmail.com>
2020-10-04 11:15:07 +0900

    테스트 케이스 추가

--- src/test/java/code/programers/level/two/BigNumberTest.java
+++ src/test/java/code/programers/level/two/BigNumberTest.java
@@ -15,4 +15,26 @@ public class BigNumberTest {
 
         assertEquals(1, index);
     }
+
+    @Test
+    void testCaseOne()
+    {
+        String str= "1924";
+        int start = 0;
+        int number = 2;
+        StringBuilder sb = new StringBuilder();
+
+        while(number > 0)
+        {
+            int index= bn.bigIndex(str.substring(start, start+ (number+1)));
+            
+            start += index+1;
+
+            sb.append(str.substring(start-1, start));
+
+            number -= index;
+        }
+
+        assertEquals("94", sb.toString());
+    }
 }
