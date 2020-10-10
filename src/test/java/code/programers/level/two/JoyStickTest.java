074778ec7f9aa9adae08d9def33bbcef459633b3
junhojava <junho9341@gmail.com>
2020-10-10 12:19:48 +0900

    문제 파악이 되지 않아서 보류

--- src/test/java/code/programers/level/two/JoyStickTest.java
+++ src/test/java/code/programers/level/two/JoyStickTest.java
@@ -3,10 +3,43 @@ package code.programers.level.two;
 import org.junit.jupiter.api.Test;
 
 /**
- * JAZ
+ * JAZ(11)
  * 
- * ↓(시작점)
+ * upKey(9)
+ *                   ↓
  * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * 
+ * leftKey(1)
+ *                                                   ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * 
+ * downKey(1)
+ * ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * 
+ * 9+1+1 = 11
+ * 
+ * 
+ * JEROEN(56)
+ * upKey(9)
+ *                   ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * downKey(5)
+ *         ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * upKey(13)
+ *                                   ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * downKey(3)
+ *                             ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * downKey(10)
+ *         ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * upKey(9)
+ *                           ↓
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ * 
  */
 public class JoyStickTest
 {
