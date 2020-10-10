69da8a14b06a510361cf1e15b87aa26d65606abb
junhojava <junho9341@gmail.com>
2020-10-10 12:04:38 +0900

    조이스틱 테스트 클래스 추가

---
+++ src/test/java/code/programers/level/two/JoyStickTest.java
@@ -0,0 +1,20 @@
+package code.programers.level.two;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * JAZ
+ * 
+ * ↓(시작점)
+ * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+ */
+public class JoyStickTest
+{
+    JoyStick js = new JoyStick();
+
+    @Test
+    void test()
+    {
+        
+    }
+}
