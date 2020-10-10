3f1d81d8aaed821f03ad928ca5fd4b5a3b501ce7
junhojava <junho9341@gmail.com>
2020-10-10 17:04:49 +0900

    생성자에서 알파벳 맵 정의와 "A"단어 생성 테스트 추가

--- src/main/java/code/programers/level/two/JoyStick.java
+++ src/main/java/code/programers/level/two/JoyStick.java
@@ -1,5 +1,39 @@
 package code.programers.level.two;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JoyStick {
-    
+    Map<String, Integer> keys;
+    String word;
+
+    JoyStick()
+    {
+        keys = new HashMap<>();
+
+        char alphabet = "A".charAt(0);
+
+        keys.put(Character.toString(alphabet++), 0);
+
+        for(int index=1; index< 14; index++)
+        {
+            keys.put(Character.toString(alphabet++), index%14);
+        }
+
+        for(int index=12; index> 0; index--)
+        {
+            keys.put(Character.toString(alphabet++), index%14);
+        }
+    }
+
+    void create(int count)
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int index=0; index< count; index++)
+        {
+            sb.append("A");
+        }
+
+        word = sb.toString();
+    }
 }
--- src/test/java/code/programers/level/two/JoyStickTest.java
+++ src/test/java/code/programers/level/two/JoyStickTest.java
@@ -1,53 +1,18 @@
 package code.programers.level.two;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-/**
- * JAZ(11)
- * 
- * upKey(9)
- *                   ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * 
- * leftKey(1)
- *                                                   ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * 
- * downKey(1)
- * ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * 
- * 9+1+1 = 11
- * 
- * 
- * JEROEN(56)
- * upKey(9)
- *                   ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * downKey(5)
- *         ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * upKey(13)
- *                                   ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * downKey(3)
- *                             ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * downKey(10)
- *         ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * upKey(9)
- *                           ↓
- * A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
- * 
- */
 public class JoyStickTest
 {
     JoyStick js = new JoyStick();
 
     @Test
-    void test()
+    void testCreateWord()
     {
-        
+        js.create(3);
+
+        assertEquals("AAA", js.word);
     }
 }
