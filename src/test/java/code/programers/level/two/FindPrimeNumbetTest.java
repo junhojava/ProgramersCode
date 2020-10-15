75afeb1997a54684c325db188d604257fd481aab
junhojava <junho9341@gmail.com>
2020-10-15 18:01:30 +0900

    테스트 케이스 추가

--- src/test/java/code/programers/level/two/FindPrimeNumbetTest.java
+++ src/test/java/code/programers/level/two/FindPrimeNumbetTest.java
@@ -1,23 +1,32 @@
 package code.programers.level.two;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import code.programers.level.one.PrimeNumber;
-
 public class FindPrimeNumbetTest {
-    
-    PrimeNumber pn = new PrimeNumber();
-    BiggestNumber bn = new BiggestNumber();
 
     @Test
-    void testSplitOne()
+    void testCaseOne()
     {
-        int[] array = new int[]{1,3,7};
+        FindPrimeNumber fpn = new FindPrimeNumber();
 
-        int[] result = FindPrimeNumber.split(array, 1);
+        int[] array = new int[]{0,1,1};
 
-        assertArrayEquals(new int[]{1,3,7}, result);
+        int count = fpn.solution(array);
+
+        assertEquals(2, count);
+    }
+
+    @Test
+    void testCaseTwo()
+    {
+        FindPrimeNumber fpn = new FindPrimeNumber();
+
+        int[] array = new int[]{1,7};
+
+        int count = fpn.solution(array);
+
+        assertEquals(3, count);
     }
 }
