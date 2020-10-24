bea8e99aa93b7bc23214fdb695ead883a04c4a6b
junhojava <junho9341@gmail.com>
2020-10-24 16:38:59 +0900

    List<Integer>를 사용하면 효율성이 떨어진다. 그냥 기본 배열로 처리함.

--- src/test/java/code/programers/level/two/LifeBoatTest.java
+++ src/test/java/code/programers/level/two/LifeBoatTest.java
@@ -31,4 +31,56 @@ public class LifeBoatTest
     {
         assertFalse(boat.isOver(20, 30, 60));
     }
+
+    @Test
+    void testCaseOne()
+    {
+        int[] array = {50,50,60,70};
+
+        List<Integer> list = boat.list(array);
+
+        int count = 0;
+
+        while(list.size() > 0)
+        {
+            if(!boat.isOver(list.get(0), list.get(list.size()-1), 100))
+                list.remove(0);
+
+            list.remove(list.size()-1);
+            count++;
+        }
+
+        assertEquals(3, count);
+    }
+
+    @Test
+    void testCaseTwo()
+    {
+        int[] array = {50, 70, 80};
+
+        List<Integer> list = boat.list(array);
+
+        int count = 0;
+        int size = array.length;
+
+        int left = 0;
+        int right = size-1;
+
+        while(size > 0)
+        {
+            if(size > 1)
+            {
+                if(!boat.isOver(array[left], array[right], 100))
+                {
+                    left++;
+                    size--;
+                }
+            }
+            right--;
+            size--;
+            count++;
+        }
+
+        assertEquals(3, count);
+    }
 }
