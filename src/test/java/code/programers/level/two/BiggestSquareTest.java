b5d83c973a4da49f02263a81eea08dfb0dcf6e1f
junhojava <junho9341@gmail.com>
2020-10-26 17:58:27 +0900

    테스트케이스 추가

--- src/test/java/code/programers/level/two/BiggestSquareTest.java
+++ src/test/java/code/programers/level/two/BiggestSquareTest.java
@@ -22,4 +22,21 @@ public class BiggestSquareTest
     {
         assertEquals(1, bs.min(1,5,3));
     }
+
+    @Test
+    void testCaseOne()
+    {
+        int[][] array = {{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,0,1}};
+        int max = 0;
+
+        for(int x=1; x<4; x++)
+            for(int y=1; y<4; y++)
+            {
+                array[x][y] = bs.min(array[x-1][y], array[x-1][y-1], array[x][y-1])+1;
+                max = (max< array[x][y]) ? array[x][y] : max;
+            }
+
+        assertEquals(3, max);
+
+    }
 }
