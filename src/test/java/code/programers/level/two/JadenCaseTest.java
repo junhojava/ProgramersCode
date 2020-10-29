f8660502a91ef608f258000a5bc7193e8e88c303
junhojava <junho9341@gmail.com>
2020-10-29 22:13:22 +0900

    JadenCase규칙을 따른 문자열 적용

--- src/test/java/code/programers/level/two/JadenCaseTest.java
+++ src/test/java/code/programers/level/two/JadenCaseTest.java
@@ -1,6 +1,7 @@
 package code.programers.level.two;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,22 @@ public class JadenCaseTest
         String str = "asdf zxcv qwer";
         String[] result = str.split(" ");
         assertArrayEquals(new String[]{"asdf", "zxcv", "qwer"}, result);
+    }
+
+    @Test
+    void testToUpperCase()
+    {
+        String str = "3asdf zxcv qwer";
+        String[] result = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(String word: result)
+        {
+            sb.append(word.substring(0,1).toUpperCase());
+            sb.append(word.substring(1,word.length()).toLowerCase()+" ");
+        }
+        sb.delete(sb.length()-1, sb.length());
+
+        assertEquals("3asdf Zxcv Qwer", sb.toString());
     }
 }
