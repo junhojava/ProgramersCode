0e471cb03bcb2f2ee2efa6be92932ab82dfaaac9
junhojava <junho9341@gmail.com>
2020-11-02 15:27:50 +0900

    피보나치의 수 0과 1일 때 테스트 추가

---
+++ src/main/java/code/programers/level/two/Fibonaci.java
@@ -0,0 +1,16 @@
+package code.programers.level.two;
+
+public class Fibonaci
+{
+    int calc(int n)
+    {
+        int result = 0;
+
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+
+        return result;
+    }
+}
---
+++ src/test/java/code/programers/level/two/FibonaciTest.java
@@ -0,0 +1,17 @@
+package code.programers.level.two;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FibonaciTest
+{
+    Fibonaci fbnc = new Fibonaci();
+
+    @Test
+    void testZeroAndOne()
+    {
+        assertEquals(0, fbnc.calc(0));
+        assertEquals(1, fbnc.calc(1));
+    }
+}
