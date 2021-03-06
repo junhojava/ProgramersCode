2b6a735780cef725022dea5d87aa8ceceb3c5cf2
junhojava <junho9341@gmail.com>
2021-03-06 23:53:24 +0900

    Arrays.sort가 Java 내장으로 지원하기 때문에 제거

--- src/main/java/code/programers/level/one/Accumulator.java
+++ src/main/java/code/programers/level/one/Accumulator.java
@@ -8,11 +8,6 @@ public class Accumulator {
 
     }
 
-    void sortLongArray(long[] longArray)
-    {
-        Arrays.sort(longArray);
-    }
-
     long sumBetweenTwoNumbers(long min, long max)
     {
         return (max - min +1) * (min + max) / 2;
--- src/test/java/code/programers/level/one/AccumulatorTest.java
+++ src/test/java/code/programers/level/one/AccumulatorTest.java
@@ -3,33 +3,24 @@ package code.programers.level.one;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 public class AccumulatorTest {
     
     @Test
-    void testBiggerNumber()
-    {
-        Accumulator accumulator = new Accumulator();
-        long[] pair = new long[]{5, 3};
-
-        accumulator.sortLongArray(pair);
-
-        assertArrayEquals(new long[]{3, 5}, pair);
-    }
-
-    @Test
     void testBetweenTwoNumbers()
     {
         Accumulator accumulator = new Accumulator();
 
-        long[] caseOne = new long[]{-2, -10};
-        long[] caseTwo = new long[]{10, 10};
+        long[] twoNTen = new long[]{-2, -10};
+        long[] tens = new long[]{10, 10};
 
-        accumulator.sortLongArray(caseOne);
-        accumulator.sortLongArray(caseTwo);
+        Arrays.sort(twoNTen);
+        Arrays.sort(tens);
 
-        assertEquals(-54, accumulator.sumBetweenTwoNumbers(caseOne[0], caseOne[1]));
-        assertEquals(10, accumulator.sumBetweenTwoNumbers(caseTwo[0], caseTwo[1]));
+        assertEquals(-54, accumulator.sumBetweenTwoNumbers(twoNTen[0], twoNTen[1]));
+        assertEquals(10, accumulator.sumBetweenTwoNumbers(tens[0], tens[1]));
     }
 }
