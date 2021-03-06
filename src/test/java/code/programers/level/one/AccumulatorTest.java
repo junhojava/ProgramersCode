d77484d3bd0110917bf940e832c53970d873ebfb
junhojava <junho9341@gmail.com>
2021-03-06 23:55:51 +0900

    테스트 코드에서 정렬할 필요 없이 상수를 입력하여 처리하도록 변경. 이게 더 간결함.

--- src/test/java/code/programers/level/one/AccumulatorTest.java
+++ src/test/java/code/programers/level/one/AccumulatorTest.java
@@ -1,9 +1,6 @@
 package code.programers.level.one;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,13 +11,7 @@ public class AccumulatorTest {
     {
         Accumulator accumulator = new Accumulator();
 
-        long[] twoNTen = new long[]{-2, -10};
-        long[] tens = new long[]{10, 10};
-
-        Arrays.sort(twoNTen);
-        Arrays.sort(tens);
-
-        assertEquals(-54, accumulator.sumBetweenTwoNumbers(twoNTen[0], twoNTen[1]));
-        assertEquals(10, accumulator.sumBetweenTwoNumbers(tens[0], tens[1]));
+        assertEquals(-54, accumulator.sumBetweenTwoNumbers(-10, -2));
+        assertEquals(10, accumulator.sumBetweenTwoNumbers(10, 10));
     }
 }
