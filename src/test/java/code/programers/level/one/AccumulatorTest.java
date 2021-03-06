31780f54f9b6c6cb85b9e88c587e26dd7b5334ad
junhojava <junho9341@gmail.com>
2021-03-06 23:59:11 +0900

    두 숫자 사이의 모든 숫자를 더하다라는게 테스트 명에 더 맞는듯 하다.

--- src/test/java/code/programers/level/one/AccumulatorTest.java
+++ src/test/java/code/programers/level/one/AccumulatorTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class AccumulatorTest {
     
     @Test
-    void testBetweenTwoNumbers()
+    void sumAllNumbersBetweenTwoNumbers()
     {
         Accumulator accumulator = new Accumulator();
 
